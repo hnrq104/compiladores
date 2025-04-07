c091728e026449bf464daffa5072ef3d99696db6
henrique <hqlc04@gmail.com>
2025-04-07 08:48:45 -0300

    first commit

---
+++ lexer.lua
@@ -0,0 +1,325 @@
+-- tables
+local reserved_words = {
+    "and", "break", "do", "else", "elseif", "end",
+    "false", "for", "function", "goto", "if", "in",
+    "local", "nil", "not", "or", "repeat", "return",
+    "then", "true", "until", "while",
+}
+
+local operators = {
+    "+", "-", "*", "/", "%", "^", "#",
+    "&", "~", "|", "<<", ">>", "//",
+    "==", "~=", "<=", ">=", "<", ">", "=",
+    "(", ")", "{", "}", "[", "]", "::",
+    ";", ":", ",", ".", "..", "..."
+}
+
+local spaces = { " ", "\t", "\n", "\r" }
+
+local function isInTable(str, table)
+    for i = 1, #table do
+        if str == table[i] then return true end
+    end
+    return false
+end
+
+-- LEXER FUNCTIONS
+
+--reads one more character, swapping positions
+local function updateLexerState(ls, iterations)
+    local n_iter = iterations or 1
+    for _ = 1, n_iter do
+        if ls.chars[1] == "\n" then
+            ls.currentLine = ls.currentLine + 1
+            ls.currentColumn = 0
+        else
+            ls.currentColumn = ls.currentColumn + 1
+        end
+
+        ls.chars[1], ls.chars[2], ls.chars[3] = ls.chars[2], ls.chars[3], io.read(1)
+    end
+end
+
+local function initLexerState()
+    local LexerState = {
+        chars = { io.read(1), io.read(1), io.read(1) },
+        currentLine = 0,
+        currentColumn = 0,
+        updateLexerState = updateLexerState -- Caviat.
+    }
+    return LexerState
+end
+
+-- AUXILIARY FUNCTIONS
+
+local function isCharInStr(char, str)
+    for i = 1, #str do
+        if char == str:sub(i, i) then return true end
+    end
+    return false
+end
+
+-- same thing as (and) or (and), i find it cleaner with if's
+local function isLetter(char)
+    local byte = string.byte(char)
+    if string.byte("a") <= byte and byte <= string.byte("z") then return true end
+    if string.byte("A") <= byte and byte <= string.byte("Z") then return true end
+    return false
+end
+
+local function isNumber(char)
+    local byte = string.byte(char)
+    return string.byte("0") <= byte and byte <= string.byte("9")
+end
+
+local function isAlphaNumeric(char)
+    return isLetter(char) or isNumber(char)
+end
+
+
+local function readCharInString(ls)
+    local escaped_table = {
+        ["\\\\"] = "\\",
+        ["\\\""] = "\"",
+        ["\\\'"] = "\'",
+        ["\\n"] = "\n",
+        ["\\r"] = "\r",
+        ["\\t"] = "\t"
+    }
+
+    if ls.chars[2] ~= nil then
+        local es = ls.chars[1] .. ls.chars[2]
+        if escaped_table[es] then
+            updateLexerState(ls, 2)
+            return escaped_table[es]
+        end
+    end
+
+    local c = ls.chars[1]
+    updateLexerState(ls)
+    return c
+end
+
+local function makeTok(tok_tag, semantic_value, start_line, start_col, end_line, end_col)
+    local tab = { Tag = tok_tag, StartLine = start_line, StartCol = start_col, EndLine = end_line, EndCol = end_col }
+    if semantic_value ~= nil then tab.Value = semantic_value end
+    return tab
+end
+
+-- READ FUNCTIONS
+local function readName(ls)
+    local name = ""
+    local start_l = ls.currentLine
+    local start_c = ls.currentColumn
+
+    while ls.chars[1] == "_" or isAlphaNumeric(ls.chars[1]) do
+        name = name .. ls.chars[1]
+        updateLexerState(ls)
+    end
+
+    if isInTable(name, reserved_words) then
+        return makeTok(string.upper(name), nil, start_l, start_c, ls.currentLine, ls.currentColumn)
+    end
+
+    return makeTok("NAME", name, start_l, start_c, ls.currentLine, ls.currentColumn)
+end
+
+local function readNumber(ls)
+    local n = ""
+    local start_l = ls.currentLine
+    local start_c = ls.currentColumn
+
+    -- hexa
+    if ls.chars[1] == '0' and isCharInStr(ls.chars[2], "xX") then
+        n = n .. '0' .. 'x'
+        updateLexerState(ls, 2)
+    end
+
+    local first_dot = true -- true until the first dot appears
+
+    local is_hexa_complete = false
+
+    while isNumber(ls.chars[1]) or isCharInStr(string.lower(ls.chars[1]), "abcdef")
+        or (first_dot and ls.chars[1] == '.') do
+        -- conditional too long, had to put a \n for luacheck :)
+
+        is_hexa_complete = true -- could optimize but really why
+        n = n .. ls.chars[1]
+        if ls.chars[1] == '.' then first_dot = false end
+        updateLexerState(ls)
+    end
+
+    if not is_hexa_complete then
+        error("Malformed hexadecimal number!")
+    end
+
+    return makeTok("NUMBER", tonumber(n), start_l, start_c, ls.currentLine, ls.currentColumn)
+end
+
+
+local function readOperator(ls)
+    -- operators are at most 3 long so we can do this gambiarra
+    for i = 3, 1, -1 do
+        local op = ""
+        if ls.chars[i] ~= nil then
+            for j = 1, i do op = op .. ls.chars[j] end
+            if isInTable(op, operators) then
+                local tok = makeTok(op, nil, ls.currentLine, ls.currentColumn, ls.currentLine, ls.currentColumn + i)
+                updateLexerState(ls, i)
+                return tok
+            end
+        end
+    end
+    return nil
+end
+
+-- read brackets returns 0 if no brackets, 1 if [[ and #eq_sign + 1 if [==...[
+local function readOpenBrackets(ls)
+    local comment_depth = 0
+    if ls.chars[1] == '[' then
+        local num_eq = 0
+        updateLexerState(ls)
+
+        while ls.chars[1] == '=' do
+            num_eq = num_eq + 1
+            updateLexerState(ls)
+        end
+
+        if ls.chars[1] == '[' then
+            comment_depth = num_eq + 1
+            updateLexerState(ls)
+        end
+    end
+    return comment_depth
+end
+
+local function readShortString(ls)
+    local start_l = ls.currentLine
+    local start_c = ls.currentColumn
+
+    local apostrophe = ls.chars[1] -- the crux of the biscuit is the apostrophe
+
+    updateLexerState(ls)
+    local str = ""
+    while not isCharInStr(ls.chars[1], apostrophe .. "\n") and ls.chars[1] ~= nil do
+        str = str .. readCharInString(ls)
+    end
+
+    if ls.chars[1] == "\n" or ls.chars[1] == nil then
+        error("Unclosed string")
+    end
+
+    updateLexerState(ls)
+    return makeTok("STRING", str, start_l, start_c, ls.currentLine, ls.currentColumn)
+end
+
+local function readBigString(ls, string_depth, start_l, start_c)
+    local str = ""
+    while true do
+        if ls.chars[1] == ']' then
+            local possible_str = "]" -- we must buffer this part, maybe what we are reading is part of the string
+
+            local finished_string = true
+            for _ = 1, string_depth - 1 do
+                updateLexerState(ls)
+                if ls.chars[1] ~= '=' then
+                    finished_string = false
+                    break
+                else
+                    possible_str = possible_str .. '='
+                end
+            end
+
+            if finished_string and ls.chars[2] == ']' then
+                updateLexerState(ls, 2)
+                return makeTok("STRING", str, start_l, start_c, ls.currentLine, ls.currentColumn)
+            else -- we didnt't finish, everything we read was a string
+                str = str .. possible_str
+            end
+        elseif ls.chars[1] == nil then
+            error(string.format("Unclosed string or comment at line %d column %d!", ls.currentLine, ls.currentColumn))
+        end
+
+        str = str .. readCharInString(ls)
+    end
+end
+
+local function readComment(ls)
+    -- skip (--)
+    updateLexerState(ls, 2)
+
+    local comment_depth = readOpenBrackets(ls)
+    -- small comment
+    if comment_depth == 0 then
+        while ls.chars[1] ~= '\n' do
+            updateLexerState(ls)
+        end
+        updateLexerState(ls)
+    else -- big comment
+        readBigString(ls, comment_depth)
+    end
+end
+
+-- the end
+local function getToken(ls)
+    -- EOF
+    if ls.chars[1] == nil then
+        return makeTok("EOF", nil, ls.currentLine, ls.currentColumn, ls.currentLine, ls.currentColumn)
+    end
+
+    -- SPACES
+    if isInTable(ls.chars[1], spaces) then
+        while isInTable(ls.chars[1], spaces) do
+            updateLexerState(ls)
+        end
+        return getToken(ls)
+    end
+
+    --COMMENTS
+    if ls.chars[1] .. ls.chars[2] == "--" then
+        readComment(ls)
+        return getToken(ls)
+    end
+
+    -- NAMES
+    if isLetter(ls.chars[1]) or (ls.chars[1] == "_" --[[ and isAlphanumeric(ls.chars[2]) ]]) then
+        return readName(ls)
+    end
+
+    -- NUMBERS
+    if isNumber(ls.chars[1]) then
+        return readNumber(ls)
+    end
+
+    -- STRINGS
+    if ls.chars[1] == [["]] or ls.chars[1] == [[']] then
+        return readShortString(ls)
+    end
+
+    if ls.chars[1] == "[" and isCharInStr(ls.chars[2], "=[") then
+        local n = readOpenBrackets(ls)
+        if n == 0 then
+            error("Big string opened improperly")
+        end
+        return readBigString(ls, n, ls.currentLine, ls.currentColumn)
+    end
+
+    -- OPERATORS
+    local tok = readOperator(ls)
+    if tok ~= nil then
+        return tok
+    end
+
+    error(string.format("Couldn't parse token: line %d col %d", ls.currentLine, ls.currentColumn))
+end
+
+local function main()
+    local ls = initLexerState()
+    print("TAG", "VALUE", "START_L", "START_C", "END_L", "END_C")
+    repeat
+        local tok = getToken(ls)
+        print(tok.Tag, tok.Value, tok.StartLine, tok.StartCol, tok.EndLine, tok.EndCol)
+    until tok.Tag == "EOF"
+end
+
+main()
